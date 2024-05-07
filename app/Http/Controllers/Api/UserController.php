a0dbb3598af2029e30942255f00731af5476a742
layn25 <ryandarmayasaok@gmail.com>
2024-05-07 12:50:24 +0800

    login api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+    
+
+        
+
     public function index()
     {
         $users = User::all();
